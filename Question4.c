8b60b125971af90b78d9953c0aca4acdb78b86b4
Florence-3108 <florence08david@gmail.com>
2026-07-25 20:17:14 +0530

    Add files via upload

---
+++ Question4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    for(int i = 5; i >= 1; i--)
+    {
+        for(int j = 5; j > i; j--)
+        {
+            printf("  ");
+        }
+        for(int j = 1; j <= i; j++)
+        {
+            if(j % 2 == 1)
+            {
+                printf("1 ");            
+            }else{
+                printf("0 ");
+            }
+    }
+    printf("\n");
+}
+return 0;
+}
